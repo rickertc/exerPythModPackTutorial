736a0a7c9266754baa8071c87a91d50c0c61801e
rickertc <u1366385@umail.utah.edu>
2021-10-24 12:04:25 -0700

    3 Files

---
+++ My Folders/fact.py
@@ -0,0 +1,11 @@
+def fact(n):
+    return 1 if n == 1 else n * fact(n-1)
+
+if (__name__ == '__main__'):
+    import sys
+    if len(sys.argv) > 1:
+        print(fact(int(sys.argv[1])))
+
+from fact import fact
+fact(6)
+720
---
+++ My Folders/importer.py
@@ -0,0 +1,6 @@
+import sys
+print(sys.path)
+
+import mod
+import fact
+print(mod)
